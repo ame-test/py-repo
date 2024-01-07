079fecd65895ca7484b86b58229d86136582cd68
Alastair <alastair.mottram-epson@digital.homeoffice.gov.uk>
2024-01-07 18:20:09 +0000

    ts ~ 2024.01.07 ~ unit test added for the zero case.

--- test_main.py
+++ test_main.py
@@ -5,3 +5,6 @@ from main import add
 class TestMain(unittest.TestCase):
     def test_main(self):
         self.assertEqual(add(1, 2), 3)
+
+    def test_main_zero_case(self):
+        self.assertEqual(add(0, 0), 0)
